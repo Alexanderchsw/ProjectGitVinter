0bf889946f96ddf4217ad919e8f2954ab5875929
Alexander <alexwins570975@gmail.com>
2023-03-10 21:41:07 +0700

    Создание осмысленного комментария для Session1 4))))

--- app/src/main/java/com/example/projectgitvinter/MainActivity.java
+++ app/src/main/java/com/example/projectgitvinter/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             //Осмысленный комментарий для Session1 1)
             //Осмысленный комментарий для Session1 2))
             //Осмысленный комментарий для Session1 3)))
+            //Осмысленный комментарий для Session1 4))))
         });
     }
 
